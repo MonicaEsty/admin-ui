f754873a160d3fad45cf7c64e304e54a03cdcc36
MonicaEsty <monicaev209@gmail.com>
2026-03-31 14:02:16 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1 className="bg-blue-200">Vite + React</h1>
       <h1>Vite + React</h1>
+      <h3>Monica Esty Vania</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
