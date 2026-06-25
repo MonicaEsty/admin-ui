3663c5b35672618091f7ba225f1a0cdde342495a
MonicaEsty <monicaev209@gmail.com>
2026-06-25 12:07:18 +0700

    latihan Prop Drilling dan latihan React Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
+import Parent from "./latihan/Parent.jsx";
+import { CounterContextProvider } from "./context/counterContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CounterContextProvider>
+      <Parent />
+    </CounterContextProvider>
+  </React.StrictMode>,
 );
